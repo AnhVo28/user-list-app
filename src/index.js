9f40cf8f91db6e96b7d05e5757a88ddf8ceb85b8
Anh Hoang Vo <vohoanganhkhtn@gmail.com>
2018-02-06 14:59:48 +0200

    Add storing local function every 2s when updating the state

--- src/index.js
+++ src/index.js
@@ -5,11 +5,24 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import reducers from './redux/reducers'
+import reducers from './redux/reducers';
+// add set time func for evoking func every defined time
+import throttle from 'lodash/throttle'
 
 
 
-const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(
+  reducers,
+  JSON.parse(localStorage.getItem('state')) || undefined,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
+const dataStringify = ()=>{
+  localStorage.setItem('state', JSON.stringify({users:store.getState().users}))
+
+}
+store.subscribe(
+  //update the localStorage every 2s
+  throttle(dataStringify, 2000)
+)
 ReactDOM.render(<Provider store ={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
